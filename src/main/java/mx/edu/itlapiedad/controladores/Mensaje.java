287313569bf6be4c03d65c4e8ccfa9ce057622a3
ZenaidaGuillen <62663527+ZenaidaGuillen@users.noreply.github.com>
2021-05-04 18:07:37 -0500

    Update Mensaje.java

--- src/main/java/mx/edu/itlapiedad/controladores/Mensaje.java
+++ src/main/java/mx/edu/itlapiedad/controladores/Mensaje.java
@@ -10,6 +10,6 @@ import org.springframework.web.bind.annotation.RestController;
 public class Mensaje {
 	@GetMapping("/hola")
 	public String mostrarHola(@RequestParam  String nombre) {
-		return "hola " + nombre;
+		return "yoaaiiiimooo" + nombre;
 	}
 }
