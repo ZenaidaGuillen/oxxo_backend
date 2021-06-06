49b59b9644c483b27f5c6961542d0f5d378cb94c
Zenaida Guillen <zenaidaguillen09@gmail.com>
2021-06-06 00:02:45 -0500

    Modificacion del 3er build

--- src/main/java/mx/edu/itlapiedad/dao/importes/ImportesJDBC.java
+++ src/main/java/mx/edu/itlapiedad/dao/importes/ImportesJDBC.java
@@ -24,7 +24,7 @@ public class ImportesJDBC implements ImportesDAO {
 
 	@Override
 	public List<Importes> consultarImportes(int id, Timestamp fecha_horai, Timestamp fecha_horaf) {
-		String sql_query = "SELECT  c.id, c.nombre, importe FROM ticket_renglones tr JOIN tickets t ON tr.TICKET_id = t.id INNER JOIN cajeros c ON c.id=t.CAJERO_id WHERE  c.id=? AND t.fecha_hora BETWEEN ? AND ?";
+		String sql_query = "SELECT  c.id, c.nombre, SUM (importe) AS total_importe FROM ticket_renglones tr JOIN tickets t ON tr.TICKET_id = t.id INNER JOIN cajeros c ON c.id=t.CAJERO_id WHERE  c.id=? AND t.fecha_hora BETWEEN ? AND ?";
 		return conexion.query(sql_query, new RowMapper<Importes>() {
 
 			@Override
@@ -32,7 +32,7 @@ public class ImportesJDBC implements ImportesDAO {
 				Importes importes = new Importes();
 				importes.setCajero_id(rs.getInt("c.id"));
 				importes.setCajero_nombre(rs.getString("c.nombre"));
-				importes.setImporte(rs.getFloat("importe"));
+				importes.setImporte(rs.getFloat("total_importe"));
 				return importes;	
 			}
 			
